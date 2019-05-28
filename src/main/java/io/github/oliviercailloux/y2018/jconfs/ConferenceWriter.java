2139a64bbf8da9ba090a9da332aee21ef4824da6
13tomoore <tom.moore@outlook.fr>
2019-05-28 22:46:41 +0200

    It's now possible to add and delete conference in an existing ics file

--- src/main/java/io/github/oliviercailloux/y2018/jconfs/ConferenceWriter.java
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/ConferenceWriter.java
@@ -1,13 +1,19 @@
 package io.github.oliviercailloux.y2018.jconfs;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.ParseException;
+
+import com.google.common.base.Preconditions;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.PropertyList;
+import net.fortuna.ical4j.model.component.CalendarComponent;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.property.CalScale;
 import net.fortuna.ical4j.model.property.ProdId;
@@ -17,14 +23,64 @@ import net.fortuna.ical4j.model.property.Version;
 import net.fortuna.ical4j.model.property.XProperty;
 import net.fortuna.ical4j.validate.ValidationException;
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.ComponentList;
 import net.fortuna.ical4j.model.Property;
 public class ConferenceWriter {
-	
+
 	public static boolean icsFileExists(String calFile) {
+		Preconditions.checkNotNull(calFile);
 		URL urlcalendar = ConferenceWriter.class.getResource(calFile+".ics");
 		return (urlcalendar!=null);
 	}
+
+	private static Calendar openCalendar(String calFile) throws IOException, ParserException {
+		Preconditions.checkNotNull(calFile);
+		URL urlcalendar = ConferenceWriter.class.getResource(calFile+".ics");
+		FileReader reader=new FileReader(new File(urlcalendar.getFile()));
+		CalendarBuilder builder = new CalendarBuilder();
+		Calendar calendar = builder.build(reader);
+		return calendar;
+	}
+
+	private static Calendar createCalendar() {
+		Calendar calendar = new Calendar();
+		calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
+		calendar.getProperties().add(Version.VERSION_2_0);
+		calendar.getProperties().add(CalScale.GREGORIAN);
+		return calendar;
+	}
 	
+	public static void deleteConference(String calFile, Conference conference) throws IOException, ParserException, NumberFormatException, ParseException, URISyntaxException {
+		
+		Calendar calendar = new Calendar();
+		Calendar newCalendar;
+		
+		Conference temp;
+		if(icsFileExists(calFile)) {
+			calendar=openCalendar(calFile);
+		}
+		else {
+			calendar=createCalendar();
+		}
+		
+		ComponentList<CalendarComponent> conflist=calendar.getComponents("X-CONFERENCE");
+		for (int i=0;i<conflist.size();i++) {
+			temp=ConferenceReader.createConference(conflist.get(i));
+			if(temp.equals(conference)) {
+				conflist.remove(i);
+			}
+		}
+		newCalendar=new Calendar(calendar);
+		try(FileOutputStream fout = new FileOutputStream(calFile+".ics")){
+			CalendarOutputter outputter = new CalendarOutputter();
+			outputter.setValidating(false);
+			outputter.output(newCalendar, fout);
+			fout.close();	 
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Write the conference in form calendar 
 	 * @param conference
@@ -34,43 +90,44 @@ public class ConferenceWriter {
 	 * @throws ValidationException
 	 * @throws URISyntaxException
 	 */
-	
+
 	public static void writeCalendarFiles(String calFile, Conference conference) throws ParseException, IOException, ParserException, ValidationException, URISyntaxException{
-		  
-		  //String calFile = "mycalendar.ics";
-		 
-		  //Creating a new calendar
-		  Calendar calendar = new Calendar();
-		  calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
-		  calendar.getProperties().add(Version.VERSION_2_0);
-		  calendar.getProperties().add(CalScale.GREGORIAN);
-		  
-		  //Creating an event
-		  PropertyList<Property> propertyList = new PropertyList<Property>();
-		 propertyList.add(new XProperty("X-DTSTART",conference.getStartDate().toString()));
-		 propertyList.add(new XProperty("X-DTEND",conference.getEndDate().toString()));
-		  
-		  propertyList.add(new Summary(conference.getTitle()));
-		  propertyList.add(new XProperty("X-COUNTRY",conference.getCountry().toString()));
-		  propertyList.add(new XProperty("X-CITY",conference.getCity().toString()));
-		  propertyList.add(new Url(conference.getUrl().toURI()));
-		  propertyList.add(new XProperty("X-FEE",conference.getFeeRegistration().toString()));
-		 
-		  VEvent meeting = new VEvent(propertyList);
-		  //add event to the calendar
-		  calendar.getComponents().add(meeting);
-		   
-		  //Saving an iCalendar file
-		  try(FileOutputStream fout = new FileOutputStream(calFile+".ics")){
-			  CalendarOutputter outputter = new CalendarOutputter();
-			  outputter.setValidating(false);
-			  outputter.output(calendar, fout);
-			  fout.close();	 
-		  }catch (IOException e) {
-				e.printStackTrace();
-		  }
-		  
-	
+
+		Calendar calendar = new Calendar();
+
+		if(icsFileExists(calFile)) {
+			calendar=openCalendar(calFile);
+		}
+		else {
+			calendar=createCalendar();
+		}
+
+		//Creating an event
+		PropertyList<Property> propertyList = new PropertyList<Property>();
+		propertyList.add(new XProperty("X-DTSTART",conference.getStartDate().toString()));
+		propertyList.add(new XProperty("X-DTEND",conference.getEndDate().toString()));
+
+		propertyList.add(new Summary(conference.getTitle()));
+		propertyList.add(new XProperty("X-COUNTRY",conference.getCountry().toString()));
+		propertyList.add(new XProperty("X-CITY",conference.getCity().toString()));
+		propertyList.add(new Url(conference.getUrl().toURI()));
+		propertyList.add(new XProperty("X-FEE",conference.getFeeRegistration().toString()));
+
+		VEvent meeting = new VEvent(propertyList);
+		//add event to the calendar
+		calendar.getComponents().add(meeting);
+
+		//Saving an iCalendar file
+		try(FileOutputStream fout = new FileOutputStream(calFile+".ics")){
+			CalendarOutputter outputter = new CalendarOutputter();
+			outputter.setValidating(false);
+			outputter.output(calendar, fout);
+			fout.close();	 
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 }
 
--- src/test/java/io/github/oliviercailloux/y2018/TestConferenceWriter.java
+++ src/test/java/io/github/oliviercailloux/y2018/TestConferenceWriter.java
@@ -2,9 +2,18 @@ package io.github.oliviercailloux.y2018;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.ParseException;
+
 import org.junit.Test;
 
+import io.github.oliviercailloux.y2018.jconfs.Conference;
 import io.github.oliviercailloux.y2018.jconfs.ConferenceWriter;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.validate.ValidationException;
 
 public class TestConferenceWriter {
 
@@ -13,6 +22,21 @@ public class TestConferenceWriter {
 		assertEquals(ConferenceWriter.icsFileExists("threeConferences"),true);
 		assertEquals(ConferenceWriter.icsFileExists("Calendartest2"),true);
 		assertEquals(ConferenceWriter.icsFileExists("stvrgtvrthb"),false);
+	}
+
+	@Test
+	public void TestAddConf() throws ParseException, ValidationException, IOException, ParserException, URISyntaxException {
+		URL u =new URL("http://example.com/");
+		Conference c = new Conference(u);
+		c.setCity("Paris");
+		c.setCountry("France");
+		c.setEndDate("06/08/2019");
+		c.setFeeRegistration(1.06);
+		c.setStartDate("06/06/2019");
+		c.setTitle("Conf des gros bonnets");
+
+		ConferenceWriter.writeCalendarFiles("bof", c);
+
 	}
 
 }
